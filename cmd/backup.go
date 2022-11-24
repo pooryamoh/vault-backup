a471ed3fb676e76b0925ab121aa89105fdb15492
Poorya <pm1375pm@gmail.com>
2022-11-24 10:14:07 +0330

    Add flags for backup command

--- cmd/backup.go
+++ cmd/backup.go
@@ -1,11 +1,13 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os/exec"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -21,7 +23,12 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("backup called")
+		dockerId, _ := cmd.Flags().GetString("dockerid")
+		volume, _ := cmd.Flags().GetString("volume")
+		dateFormat, _ := cmd.Flags().GetString("dateformat")
+		rclonePath, _ := cmd.Flags().GetString("rclonepath")
+
+		_ = backup(dockerId, volume, dateFormat, rclonePath)
 	},
 }
 
@@ -37,10 +44,43 @@ func init() {
 	backupCmd.PersistentFlags().String("dateformat", "f", "Date format of filename")
 	backupCmd.PersistentFlags().String("rclonepath", "p", "Path of rclone remote \"remotename:path\"")
 
-
-
-
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// backupCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func backup(dockerId string, volume string, dateFormat string, rclonePath string) bool {
+
+	command := fmt.Sprintf("docker stop %s", dockerId)
+	_, err := exec.Command("bash", "-c", command).Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dt := time.Now()
+	filename := fmt.Sprint(dt.Format(dateFormat) + ".tar")
+	command = fmt.Sprintf("tar -cf %s %s", filename, volume)
+	_, err = exec.Command("bash", "-c", command).Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	command = fmt.Sprintf("rclone copy /tmp/%s %s", filename, rclonePath)
+
+	_, err = exec.Command("bash", "-c", command).Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	command = fmt.Sprintf("docker start %s", dockerId)
+	_, err = exec.Command("bash", "-c", command).Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return true
 }
